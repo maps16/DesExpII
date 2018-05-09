7d5bc5cc61d8dbf8bd6441e4af9900748978354a
Martin Alejandro Paredes Sosa <mapsjr16@gmail.com>
2018-05-09 16:03:52 -0700

    Run

--- Proyecto_MC/Corridas/Run.sh
+++ Proyecto_MC/Corridas/Run.sh
@@ -8,10 +8,23 @@
 ## Started on  Wed May  9 10:50:21 2018 Martin Alejandro Paredes Sosa
 ## Last update Time-stamp: <2010-oct-11.lunes 17:30:06 (calcaneo)>
 ##
-IFS=":"
-LIST="01:02:03:04:05:06:07:08:09"
-
-for i in $LIST ; do
-    /usr/bin/time ./SimularHS$i >&Salida$i &
-    /bin/sleep 2
-done
+/usr/bin/time ./SimularHS01 >&Salida01&
+/bin/sleep 2
+/usr/bin/time ./SimularHS02 >&Salida02&
+/bin/sleep 2
+/usr/bin/time ./SimularHS03 >&Salida03&
+/bin/sleep 2
+/usr/bin/time ./SimularHS04 >&Salida04&
+/bin/sleep 2
+/usr/bin/time ./SimularHS05 >&Salida05&
+/bin/sleep 2
+/usr/bin/time ./SimularHS06 >&Salida06&
+/bin/sleep 2
+/usr/bin/time ./SimularHS07 >&Salida07&
+/bin/sleep 2
+/usr/bin/time ./SimularHS08 >&Salida08&
+/bin/sleep 2
+/usr/bin/time ./SimularHS09 >&Salida09&
+/bin/sleep 2
+#usr/bin/time ./SimularHS10 >&Salida10&
+#bin/sleep 2
