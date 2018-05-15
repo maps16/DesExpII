f237b6da85c4ee224b86eabcf91d6dee38fa7cdc
Martin Alejandro Paredes Sosa <mapsjr16@gmail.com>
2018-05-15 16:50:30 -0700

    l

--- Portafolio_II/Tarea_IV/Compile.sh
+++ Portafolio_II/Tarea_IV/Compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-gfortran Mod.f03 ConfigIni.f03 ConfigIniReg.f03 Act7.f03 EnergyConfigHD.f03 EnergyPartHD.f03 -o Simular
+gfortran Mod.f03 ConfigIni.f03 ConfigIniReg.f03 ConfigIniTras.f03 Act7.f03 EnergyConfigHD.f03 EnergyPartHD.f03 -o Simular
 ./clean.sh
